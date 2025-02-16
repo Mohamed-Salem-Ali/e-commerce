e705d23af178a708e386d43d7f6de853bd258a46
Mohamed Salem Ali <mohamedsalem0100411@sci.asu.edu.eg>
2025-02-16 18:33:18 +0200

    feat(api): add profile endpoint for user data retrieval and update
    - Created UserProfileView using generics
    - Integrated UserProfileSerializer
    - Restricted access with IsAuthenticated permission
    - Added profile/ route in urls.py

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
 
+from authentication.views import UserProfileView
+
 urlpatterns = [
     path('auth/', include('dj_rest_auth.urls')),  # Login, Logout, Password Reset
     path('auth/registration/', include('dj_rest_auth.registration.urls')),  # Register
+    path('auth/profile/', UserProfileView.as_view(), name='user-profile'),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from rest_framework import generics, permissions
+from .models import Customer
+from .serializers import UserProfileSerializer
 # Create your views here.
+class UserProfileView(generics.RetrieveUpdateAPIView):
+    queryset = Customer.objects.all()
+    serializer_class = UserProfileSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
