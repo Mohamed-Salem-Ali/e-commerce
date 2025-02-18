a74e0c734556cf657237a093fd50b39d47d6ef39
Mohamed Salem Ali <mohamedsalem0100411@sci.asu.edu.eg>
2025-02-19 01:53:32 +0200

    Feat(Auth): Login and Logout Configuration - Edit "urls.py" by adding the new paths for auth pages - Add register, login and logout functions - Add message to user and logout button in Navbar - Add styling for user hello message

--- authentication/views.py
+++ authentication/views.py
@@ -1,25 +1,40 @@
-from django.shortcuts import render
-from rest_framework import generics, permissions
-from .models import Customer
-from .serializers import UserProfileSerializer
-# Create your views here.
-class UserProfileView(generics.RetrieveUpdateAPIView):
-    queryset = Customer.objects.all()
-    serializer_class = UserProfileSerializer
-    permission_classes = [permissions.IsAuthenticated]
+from pyexpat.errors import messages
+from django.shortcuts import render ,redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from .forms import CreateUserForm
+from django.contrib import messages
 
-    def get_object(self):
-        return self.request.user
-    
-def register(request):
+def registerPage(request):
+    form = CreateUserForm()
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user_name = form.cleaned_data.get('username')
+            messages.success(request, 'User was created for '+user_name) 
+
+            return redirect('login')
+    context={'form': form}
+    return render(request,'authentication/register.html',context)
+
+def loginPage(request):
+    if request.method == 'POST':
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+
+        user = authenticate(request,username=username,password=password)
+        if user is not None:
+            login(request,user)
+            return redirect('home')
+        else:
+            messages.info(request, 'username or password is incorrect')
     context={}
-    return render(request,'authentication/register.html')
+    return render(request,'authentication/login.html',context)
 
-
-def login(request):
-    context={}
-    return render(request,'authentication/login.html')
-
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
 
 def profile(request):
     context={}
