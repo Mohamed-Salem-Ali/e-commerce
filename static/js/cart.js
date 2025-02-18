2e0dd5087b31f216d6b6df3072d1ef118483210d
Mohamed Salem Ali <mohamedsalem0100411@sci.asu.edu.eg>
2025-02-19 00:25:17 +0200

    Feat(Auth): Create pages for register and login - Create forms.py file to add fields in "UserCreationForm" - Create Register page with django forms - User register using username, email, password - Create Login page with django forms - User login using username and password - Adding style for authentication pages - Change the way to use "CSRFToken" in Post request from use variable to use "getCookie" function

--- static/js/cart.js
+++ static/js/cart.js
@@ -26,7 +26,7 @@ function updateUserOrder(productId, action){
 			method:'POST',
 			headers:{
 				'Content-Type':'application/json',
-                'X-CSRFToken':csrftoken,
+                'X-CSRFToken':getCookie('csrftoken'),
 			}, 
 			body:JSON.stringify({'productId':productId, 'action':action})
 		})
