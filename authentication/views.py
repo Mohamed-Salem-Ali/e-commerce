9ea959748f3df652bb828caa0c1b25918c7ed3f9
Mohamed Salem Ali <mohamedsalem0100411@sci.asu.edu.eg>
2025-02-19 05:32:10 +0200

    Feat(Core):  Create profile page & Update user data - Add "CustomerForm" to "forms.py" - Create Profile function in "views.py" with logic of updating customer data - Adding conditions to handle using same username - Create Profile page with form to update user data - Make profile, name and logout buttons appears to authenticated users only

--- authentication/views.py
+++ authentication/views.py
@@ -1,11 +1,12 @@
 from pyexpat.errors import messages
 from django.shortcuts import render ,redirect
-from django.contrib.auth.models import Group
+from django.contrib.auth.models import Group ,User
 from django.contrib.auth import authenticate, login, logout
-from .forms import CreateUserForm
+from core.models import Order
+from .forms import CreateUserForm, CustomerForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .decrators import unauthenticated_user
+from .decrators import allowed_users, unauthenticated_user
 from .models import Customer
 
 @unauthenticated_user
@@ -23,7 +24,7 @@ def registerPage(request):
             Customer.objects.create(
                 user=user,
                 username=user_name,
-                email=email  # Ensure email is saved
+                email=email  
             )
             messages.success(request, 'User was created for '+user_name) 
 
@@ -51,6 +52,25 @@ def logoutUser(request):
     return redirect('login')
 
 @login_required(login_url='login')
+@allowed_users(allowed_roles=['customer'])
 def profile(request):
-    context={}
-    return render(request,'authentication/profile.html')
+    customer = request.user.customer
+    order, created = Order.objects.get_or_create(customer=customer, complete=False)
+    cartItems = order.get_cart_items
+    form = CustomerForm(instance=customer)
+
+    if request.method == 'POST':
+        form = CustomerForm(request.POST, request.FILES, instance=customer)
+        if form.is_valid():
+            new_username = form.cleaned_data.get('username')
+
+            if User.objects.filter(username=new_username).exclude(id=request.user.id).exists():
+                messages.error(request, 'This username is already taken')
+            else:
+                customer = form.save()  
+                request.user.username = new_username  
+                request.user.save()  
+                messages.success(request, 'Profile updated successfully')
+
+    context = {'form': form, 'order':order, 'cartItems':cartItems}
+    return render(request, 'authentication/profile.html', context)
